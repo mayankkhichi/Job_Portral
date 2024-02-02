5083fa4a61e836c8306805bd23122f20922562a3
mayankkhichi <117597630+mayankkhichi@users.noreply.github.com>
2024-02-02 23:55:36 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>HELLO WORLD</h1>
+      <h1>HELLO WORLD </h1>
     </>
   )
 }
